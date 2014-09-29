2fa13e20091c070d3c1913e917456e5b588b7241
Carl Walsh <darthwalsh@gmail.com>
2014-09-29 00:59:13 -0700

    nuget install scripts

---
+++ StatusServer/tools/install.ps1
@@ -0,0 +1,4 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$project.ProjectItems.Item("views").ProjectItems.Item("details.html").Properties.Item("CopyToOutputDirectory").Value = 1
+$project.ProjectItems.Item("views").ProjectItems.Item("index.html").Properties.Item("CopyToOutputDirectory").Value = 1
