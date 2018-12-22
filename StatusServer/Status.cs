5bc37c203247953d0c5a4088d6ce22dbdfc54253
Carl Walsh <darthwalsh@gmail.com>
2018-12-22 12:14:59 -0800

    undo formatting
    formatting was applied to only Status, reformat to match other code

--- StatusServer/Status.cs
+++ StatusServer/Status.cs
@@ -17,8 +17,7 @@ namespace StatusServer
     //TODO JSON foramt
     class StatusData : IEquatable<StatusData>, IData
     {
-        public StatusData(string errorMessage = null)
-        {
+        public StatusData(string errorMessage = null) {
             this.DateTime = DateTime.Now;
             this.ErrorMessage = errorMessage;
         }
@@ -27,57 +26,45 @@ namespace StatusServer
         public string ErrorMessage { get; private set; }
         public bool HadError { get { return this.ErrorMessage != null; } }
 
-        public string Serialize()
-        {
+        public string Serialize() {
             string result = "" + this.DateTime.Ticks;
-            if (this.ErrorMessage != null)
-            {
+            if (this.ErrorMessage != null) {
                 result += " " + this.ErrorMessage.Replace("\\", @"\\").Replace("\r", @"\r").Replace("\n", @"\n");
             }
             return result;
         }
 
-        public static StatusData Deserialize(string serialized)
-        {
+        public static StatusData Deserialize(string serialized) {
             int index = serialized.IndexOf(" ");
-            if (index == -1)
-            {
+            if (index == -1) {
                 return new StatusData { DateTime = DateTime.FromBinary(long.Parse(serialized)) };
             }
-            return new StatusData
-            {
+            return new StatusData {
                 DateTime = DateTime.FromBinary(long.Parse(serialized.Substring(0, index))),
                 ErrorMessage = serialized.Substring(index + 1).Replace(@"\n", "\n").Replace(@"\r", "\r").Replace(@"\\", "\\")
             };
         }
 
-        public static StatusData TryDeserialize(string serialized)
-        {
-            try
-            {
+        public static StatusData TryDeserialize(string serialized) {
+            try {
                 return Deserialize(serialized);
-            }
-            catch
-            {
+            } catch {
                 return null;
             }
         }
 
-        public override int GetHashCode()
-        {
+        public override int GetHashCode() {
             int hash = this.DateTime.GetHashCode();
             if (this.ErrorMessage != null)
                 hash ^= this.ErrorMessage.GetHashCode();
             return hash;
         }
 
-        public override bool Equals(object other)
-        {
+        public override bool Equals(object other) {
             return Equals(other as StatusData);
         }
 
-        public bool Equals(StatusData other)
-        {
+        public bool Equals(StatusData other) {
             return other != null
                 && this.DateTime == other.DateTime
                 && this.ErrorMessage == other.ErrorMessage;
@@ -87,18 +74,18 @@ namespace StatusServer
     public abstract class Status
     {
 #if DEBUG
-        static readonly TimeSpan defaultWait = TimeSpan.FromSeconds(1); 
+        static readonly TimeSpan defaultWait = TimeSpan.FromSeconds(1);
 #else
 		static readonly TimeSpan defaultWait = TimeSpan.FromMinutes(5);
 #endif
 
         static Dictionary<string, Status> all = new Dictionary<string, Status>();
 
-		internal static Dictionary<string, Status> All {
-			get {
-				return all;
-			}
-		}
+        internal static Dictionary<string, Status> All {
+            get {
+                return all;
+            }
+        }
 
         public static void Cleanup(TimeSpan timespan) {
             Directory.CreateDirectory(StatusServerPath);
@@ -113,10 +100,8 @@ namespace StatusServer
             }
         }
 
-        public static void Initialize(IEnumerable<Status> stati)
-        {
-            if (all.Any())
-            {
+        public static void Initialize(IEnumerable<Status> stati) {
+            if (all.Any()) {
                 throw new Exception("Already Intialized!");
             }
             all = stati.ToDictionary(s => s.Name);
@@ -125,8 +110,7 @@ namespace StatusServer
                 s.Start();
         }
 
-        public static void Initialize()
-        {
+        public static void Initialize() {
             Initialize(AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(a => a.GetTypes())
                 .Where(t => t.IsSubclassOf(typeof(Status)))
@@ -135,8 +119,7 @@ namespace StatusServer
                 .Cast<Status>());
         }
 
-        public static void ShutDown()
-        {
+        public static void ShutDown() {
             List<Status> toDispose = all.Values.ToList();
             all = new Dictionary<string, Status>();
 
@@ -149,20 +132,15 @@ namespace StatusServer
 
         public static event Action<Status> OnFailure = delegate { };
 
-        public static void WaitAll()
-        {
-            using (var e = new CountdownEvent(all.Count))
-            {
-                foreach (var s in all.Values)
-                {
-                    lock (s.padLock)
-                    {
+        public static void WaitAll() {
+            using (var e = new CountdownEvent(all.Count)) {
+                foreach (var s in all.Values) {
+                    lock (s.padLock) {
                         s.finishCallbacks.Enqueue(() => e.Signal());
                     }
                 }
 
-                foreach (var s in all.Values)
-                {
+                foreach (var s in all.Values) {
                     s.verifyWait.Set();
                 }
 
@@ -185,12 +163,10 @@ namespace StatusServer
         readonly TimeSpan delay;
 
         protected Status()
-            : this(defaultWait)
-        {
+            : this(defaultWait) {
         }
 
-        protected Status(TimeSpan d)
-        {
+        protected Status(TimeSpan d) {
             this.delay = d;
             string name = GetType().Name;
 
@@ -200,8 +176,7 @@ namespace StatusServer
             this.Name = name;
 
             this.History = ImmutableStack<StatusData>.Empty;
-            if (Directory.Exists(StatusServerPath))
-            {
+            if (Directory.Exists(StatusServerPath)) {
                 this.History = ImmutableStack<StatusData>.New(
                     Directory.EnumerateFiles(StatusServerPath, this.Name + ".txt", SearchOption.AllDirectories)
                         .SelectMany(File.ReadAllLines)
@@ -210,62 +185,49 @@ namespace StatusServer
                         .OrderBy(data => data.DateTime));
             }
 
-            this.verifyThread = new Thread(() =>
-            {
-                while (true)
-                {
+            this.verifyThread = new Thread(() => {
+                while (true) {
                     verifyWait.WaitOne(this.delay);
 
-                    lock (this.padLock)
-                    {
+                    lock (this.padLock) {
                         if (this.stop)
                             break;
                     }
 
                     StatusData data;
-                    try
-                    {
+                    try {
                         Verify();
                         data = new StatusData();
-                    }
-                    catch (Exception e)
-                    {
+                    } catch (Exception e) {
                         data = new StatusData(e.ToString());
                     }
 
                     Log(data);
-                    if (data.HadError)
-                    {
+                    if (data.HadError) {
                         OnFailure(this);
                     }
 
-                    lock (this.padLock)
-                    {
+                    lock (this.padLock) {
                         while (this.finishCallbacks.Any())
                             this.finishCallbacks.Dequeue()();
                     }
                 }
-            })
-            {
+            }) {
                 IsBackground = true
             };
 
             DateTime startedAt = DateTime.Now;
-            this.hungThread = new Thread(() =>
-            {
-                while (true)
-                {
+            this.hungThread = new Thread(() => {
+                while (true) {
                     hungWait.WaitOne(this.HangDelay);
 
-                    lock (this.padLock)
-                    {
+                    lock (this.padLock) {
                         if (this.stop)
                             break;
                     }
 
                     StatusData first = this.History.FirstOrDefault();
-                    if (first == null)
-                    {
+                    if (first == null) {
                         continue;
                     }
 
@@ -273,89 +235,76 @@ namespace StatusServer
 
                     TimeSpan lastResult = now - first.DateTime;
 
-                    if (lastResult > this.HangDelay && now > (startedAt + this.HangDelay))
-                    {
+                    if (lastResult > this.HangDelay && now > (startedAt + this.HangDelay)) {
                         Log(new StatusData("Evaluation didn't finish within exected time."));
                         OnFailure(this);
                     }
                 }
-            })
-            {
+            }) {
                 IsBackground = true
             };
         }
 
-        void Start()
-        {
+        void Start() {
             this.verifyThread.Start();
             this.hungThread.Start();
         }
 
-        void Stop()
-        {
-            lock (this.padLock)
-            {
+        void Stop() {
+            lock (this.padLock) {
                 this.stop = true;
             }
             this.verifyWait.Set();
             this.hungWait.Set();
         }
 
-        void Join()
-        {
+        void Join() {
             TimeSpan printDelay = new TimeSpan(100);
-            if (!this.verifyThread.Join(printDelay))
-            {
+            if (!this.verifyThread.Join(printDelay)) {
                 Console.WriteLine("Waiting for {0} verify", this.Name);
                 this.verifyThread.Join();
             }
-            if (!this.hungThread.Join(printDelay))
-            {
+            if (!this.hungThread.Join(printDelay)) {
                 Console.WriteLine("Waiting for {0} hung", this.Name);
                 this.hungThread.Join();
             }
         }
 
-        void Log(StatusData data)
-        {
-            lock (this.padLock)
-            {
+        void Log(StatusData data) {
+            lock (this.padLock) {
                 this.History = this.History.Push(data);
-                using (var writer = new StreamWriter(this.SavePath, append: true))
-                {
+                using (var writer = new StreamWriter(this.SavePath, append: true)) {
                     writer.WriteLine(data.Serialize());
                 }
             }
         }
 
-        internal static string StatusServerPath
-        {
-            get
-            {
+        internal static string StatusServerPath {
+            get {
                 return Path.Combine(
                     Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
                     "StatusServer");
             }
         }
 
-		internal static string StatusServerOldPath {
-			get {
-				return Path.Combine(
-					Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-					"StatusServerOld");
-			}
-		}
+        internal static string StatusServerOldPath {
+            get {
+                return Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "StatusServerOld");
+            }
+        }
 
-		internal string SavePath {
-			get {
-				var dir = Path.Combine(StatusServerPath,
-					DateTime.Now.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
-				Directory.CreateDirectory(dir);
-				return Path.Combine(dir, this.Name + ".txt");
-			}
-		}
-		
-		internal IStack<StatusData> History { get; private set; }
+        internal string SavePath {
+            get {
+                var dir = Path.Combine(StatusServerPath,
+                    DateTime.Now.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+                Directory.CreateDirectory(dir);
+                return Path.Combine(dir, this.Name + ".txt");
+            }
+        }
+
+        internal IStack<StatusData> History { get; private set; }
 
         public IEnumerable<IData> ErrorHistory { get { return this.History; } }
 
@@ -364,10 +313,8 @@ namespace StatusServer
         //TODO return an IData, avoid stack trace?
         protected abstract void Verify();
 
-        protected virtual TimeSpan HangDelay
-        {
-            get
-            {
+        protected virtual TimeSpan HangDelay {
+            get {
                 return this.delay + this.delay + TimeSpan.FromSeconds(2);
             }
         }
@@ -376,18 +323,15 @@ namespace StatusServer
     static class MinimizeExtensions
     {
         // Removes duplicates, e.g. 0, 0, 0, 1, 1, 1 becomes 0, 0, 1, 1
-        internal static IEnumerable<T> Minimize<T>(this IEnumerable<T> data, IEqualityComparer<T> comparer)
-        {
+        internal static IEnumerable<T> Minimize<T>(this IEnumerable<T> data, IEqualityComparer<T> comparer) {
             T past = default(T), present = default(T), future = default(T);
-            using (var it = data.GetEnumerator())
-            {
+            using (var it = data.GetEnumerator()) {
 
                 if (!it.MoveNext())
                     yield break;
                 future = it.Current;
 
-                if (!it.MoveNext())
-                {
+                if (!it.MoveNext()) {
                     yield return future;
                     yield break;
                 }
@@ -395,8 +339,7 @@ namespace StatusServer
                 future = it.Current;
                 yield return present;
 
-                while (it.MoveNext())
-                {
+                while (it.MoveNext()) {
                     past = present;
                     present = future;
                     future = it.Current;
@@ -409,8 +352,7 @@ namespace StatusServer
             yield return future;
         }
 
-        internal static IEnumerable<T> Minimize<T>(this IEnumerable<T> data)
-        {
+        internal static IEnumerable<T> Minimize<T>(this IEnumerable<T> data) {
             return data.Minimize(EqualityComparer<T>.Default);
         }
     }
